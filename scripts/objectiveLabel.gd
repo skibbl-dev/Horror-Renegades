180a3205949ec2fe4607706949ef1f6dd9c258c0
dibin_17 <dylan@rut66.net>
2026-05-30 17:53:26 -0600

    Finished objective. now what bro?

--- scripts/objectiveLabel.gd
+++ scripts/objectiveLabel.gd
@@ -1,19 +1,22 @@
 extends Label
 
 var current_tween: Tween
+var can_show_objective := true
 
 func _ready():
 	modulate.a = 0.0
 	Objective.objective_changed.connect(_on_objective_changed)
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("show_objectives"):
+	if Input.is_action_just_pressed("show_objectives") and can_show_objective:
 		show_objective(Objective.current_objective)
 
 func _on_objective_changed(new_text: String):
 	show_objective(new_text)
 
 func show_objective(new_text: String):
+	can_show_objective = false
+
 	text = new_text
 
 	if current_tween:
@@ -22,9 +25,9 @@ func show_objective(new_text: String):
 	modulate.a = 0.0
 
 	current_tween = create_tween()
-
 	current_tween.tween_property(self, "modulate:a", 1.0, 0.5)
-
 	current_tween.tween_interval(4.0)
-
 	current_tween.tween_property(self, "modulate:a", 0.0, 0.5)
+
+	await current_tween.finished
+	can_show_objective = true
