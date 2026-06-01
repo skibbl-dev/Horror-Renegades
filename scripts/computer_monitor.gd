66b057824613a64fa17267d4c28d048b0fbc5ecb
sᴋɪʙʙʟ_ <62254455+skibbl-dev@users.noreply.github.com>
2026-06-01 10:00:01 -0600

    computer bug fixes + rename

--- scripts/computer_monitor.gd
+++ scripts/computer_monitor.gd
@@ -4,11 +4,13 @@ extends Node3D
 @onready var screen_mesh: MeshInstance3D = $Screen
 @onready var player_ray: RayCast3D = get_tree().get_nodes_in_group("player")[0].raycast
 @onready var cursor: Marker3D = $Cursor
+@onready var screen_area: Area3D = $ScreenArea
 
 func _ready() -> void:
 	screen_mesh.get_mesh().get_material().albedo_texture = viewport.get_texture()
 
 func _process(_delta: float) -> void:
 	if player_ray.is_colliding():
-		var collision_point = player_ray.get_collision_point()
-		cursor.global_position = cursor.global_position.lerp(collision_point, 0.3)
+		if(player_ray.get_collider() == screen_area):
+			var collision_point = player_ray.get_collision_point()
+			cursor.global_position = cursor.global_position.lerp(collision_point, 0.3)
